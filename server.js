6c5ae4f0d425c70d748b4eac7b3eba9835c28c95
Abi Beggs <beggsaj@gmail.com>
2022-01-17 13:57:59 -0600

    updated paths

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const path = require('path')
 const express = require('express')
 const app = express()
-const completeNotes = require('./db.json')
+const completeNotes = require('./Develop/db/db.json')
 const { text } = require('body-parser')
 
 
